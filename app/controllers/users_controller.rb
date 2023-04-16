1dd454c15272e73e7fb1487f74329a90c8f38b17
annanakam <124420731+annanakam@users.noreply.github.com>
2023-04-16 14:19:51 +0000

    [stop]bookers2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,18 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     @books = @user.books
   end
-
+  
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    @book.save
+    redirect_to book_path_params
+  end
+    
   def edit
+  end
+  
+  def book_path_params
+    params.require()
   end
 end
